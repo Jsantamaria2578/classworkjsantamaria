c639f3cf50c79d777e082c5f2806df9e5c3c3322
BT_1N3_12 <BT_1N3_12@BT_1N3_12.bktech.local>
2018-01-03 13:11:53 -0500

    concepts - done

--- jsantamariaclasswork/src/whackAMole/setStage.java
+++ jsantamariaclasswork/src/whackAMole/setStage.java
@@ -9,23 +9,54 @@ import javafx.stage.Stage;
 import javax.swing.*;
 import javafx.scene.layout.GridPane;
 import javafx.scene.shape.*;
+import javafx.scene.layout.FlowPane;
+import java.lang.*;
 public class setStage extends Application {
 
 	public void start(Stage primaryStage) throws Exception{
 		 
 		primaryStage.setTitle("Whack a mole");
 		Button button = new Button("dirty mole");
-	/*	button.setMaxSize(145.00, 75.00);
-		button.setMinSize(145.00, 75.00);
-		button.setPrefSize(145.00, 75.00);*/
-		button.setShape(new Circle((2)));
-		button.setMaxSize(100,100);
-		button.setMinSize(100,100);
+		button.setMaxSize(1200,10); // this will be the dimensions if the button is at the top
+		button.setMinSize(1200,10);
 		Label label = new Label("");
-		HBox hbox = new HBox(button,label);
+	//	HBox hbox = new HBox(button,label);
 		
+		Button button1 = new Button("dirty mole");
+		button1.setShape(new Circle((2)));
+		button1.setMaxSize(100,100);
+		button1.setMinSize(100,100);
+		FlowPane flowpane = new FlowPane();
 
-		Scene potato = new Scene(hbox,1200,800);
+		flowpane.getChildren().add(button);
+		flowpane.getChildren().add(button1);
+		
+		//button.setVisible(false);
+		//button1.setVisible(false);
+		// if i have to make visibility differences the buttons will be relative to last? YES... 
+		// flowpane.getChildren().clear(); THIS WORKS 
+		flowpane.setVgap(250);
+		flowpane.setHgap(250);
+		
+		button1.setOnAction(value ->  {
+	           /*button1.setVisible(false);
+	           flowpane.getChildren().clear();
+	           flowpane.getChildren().add(button);
+	           flowpane.getChildren().add(button1);				// this works but it may be a bit excessive. can i just change the gap midway 
+	           flowpane.setVgap(100);								yes yes i can Q-Q
+	           flowpane.setHgap(100);
+	           button1.setVisible(true);
+	           primaryStage.show();*/
+			
+			flowpane.setStyle("-fx-alignment : top-left");
+			double verticalGap = Math.random() *690;// correct max based off top height and width
+			double horizontalGap = Math.random() *600;			// solution: create a random set style, set button one to either the entire top or left hand edge of the screen
+			flowpane.setVgap(verticalGap);						// then create a vgap if its at the top or hgap if its and the left
+	           flowpane.setHgap(0);								// basically creating random intervals
+	           
+	        });
+		
+		Scene potato = new Scene(flowpane,1200,800);
 		primaryStage.setScene(potato);
 		primaryStage.show();
 	}
